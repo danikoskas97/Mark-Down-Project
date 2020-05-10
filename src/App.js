b8759ab3179c18039d5b1bf294c9c582c4dbf733
dani koskas <koskas.dani@gmail.com>
2020-05-10 21:30:57 +0200

    late night work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, } from 'react';
 import './App.css';
+import marked from 'marked'
+import { sampleText } from './sampleText'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text:sampleText
+  }
+
+  handelChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => marked(text, { sanitize: true })
+
+  render(){
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-6'>
+          <textarea
+          onChange={this.handelChange}
+            value={this.state.text}
+            rows='35' 
+            className='form-control'/>
+          </div>
+          <div className='col-sm-6'>
+            <div dangerouslySetInnerHTML={{ __html: this.renderText(this.state.text) }} />
+          </div>
+        </div>
+        
+          
+        
+      </div>  
+    
+      
+    );
+  }
 }
 
 export default App;
